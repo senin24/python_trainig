36d2caf3fcd9240459d1d0eff3d14906d217ff0f
Senin, Pavel <Pavel.Senin@Kronos.com>
2017-07-30 20:55:38 +0300

    add class Group

--- test_add_group.py
+++ test_add_group.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
+from  group import Group
 import unittest
 
 
@@ -27,11 +28,11 @@ class test_add_group(unittest.TestCase):
     def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
 
-    def create_group(self, wd, name, header, footer):
+    def create_group(self, wd, group):
         wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").send_keys(name)
-        wd.find_element_by_name("group_header").send_keys(header)
-        wd.find_element_by_name("group_footer").send_keys(footer)
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         wd.find_element_by_name("submit").click()
 
     def return_to_group_page(self, wd):
@@ -46,7 +47,7 @@ class test_add_group(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd, name="python_test_group", header="python_test_group_Logo", footer="python_test_group_Comment")
+        self.create_group(wd, Group(name="python_test_group", header="python_test_group_Logo", footer="python_test_group_Comment"))
         self.return_to_group_page(wd)
         self.logout(wd)
         self.assertTrue(success)
@@ -57,7 +58,7 @@ class test_add_group(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd, name="", header="", footer="")
+        self.create_group(wd, Group(name="", header="", footer=""))
         self.return_to_group_page(wd)
         self.logout(wd)
         self.assertTrue(success)
