ce976d093d3bca5ef88d5af45dda58239e99e540
Pavel Senin <psenin@mail.ru>
2017-07-30 19:44:48 +0300

    add capabilities, remove unused code

--- test_add_group.py
+++ test_add_group.py
@@ -3,6 +3,7 @@ from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.common.action_chains import ActionChains
 import time, unittest
 
+
 def is_alert_present(wd):
     try:
         wd.switch_to_alert().text
@@ -10,31 +11,23 @@ def is_alert_present(wd):
     except:
         return False
 
+
 class test_add_group(unittest.TestCase):
     def setUp(self):
-        self.wd = WebDriver()
+        self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
     
     def test_test_add_group(self):
         success = True
         wd = self.wd
         wd.get("http://192.168.10.214/addressbook/index.php")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").send_keys("\\undefined")
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath(".//*[@id='LoginForm']/input[@value='Login']").click()
         wd.find_element_by_link_text("groups").click()
         wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("python_test_group")
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
         wd.find_element_by_name("group_header").send_keys("python_test_group_Logo")
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("python_test_group_Comment")
         wd.find_element_by_name("submit").click()
         wd.find_element_by_link_text("group page").click()
