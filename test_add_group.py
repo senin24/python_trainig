631a8038b5bc671682cc44d42302c97589228789
Senin, Pavel <Pavel.Senin@Kronos.com>
2017-07-30 20:27:55 +0300

    separate to methods

--- test_add_group.py
+++ test_add_group.py
@@ -15,24 +15,42 @@ class test_add_group(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
-    
-    def test_test_add_group(self):
-        success = True
-        wd = self.wd
+
+    def open_home_page(self, wd):
         wd.get("http://192.168.10.214/addressbook/index.php")
+
+    def login(self, wd):
         wd.find_element_by_name("user").send_keys("admin")
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath(".//*[@id='LoginForm']/input[@value='Login']").click()
+
+    def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
+
+    def create_group(self, wd):
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").send_keys("python_test_group")
         wd.find_element_by_name("group_header").send_keys("python_test_group_Logo")
         wd.find_element_by_name("group_footer").send_keys("python_test_group_Comment")
         wd.find_element_by_name("submit").click()
+
+    def return_to_group_page(self, wd):
         wd.find_element_by_link_text("group page").click()
+
+    def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
+
+    def test_test_add_group(self):
+        success = True
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_groups_page(wd)
+        self.create_group(wd)
+        self.return_to_group_page(wd)
+        self.logout(wd)
         self.assertTrue(success)
-    
+
     def tearDown(self):
         self.wd.quit()
 
