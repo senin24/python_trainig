32f630499a69f7099a26cabdebdac025d8d00e70
Pavel Senin <psenin@mail.ru>
2017-08-12 15:44:32 +0300

    add Class for fixture

---
+++ application.py
@@ -0,0 +1,58 @@
+from selenium.webdriver.firefox.webdriver import WebDriver
+
+
+class Application:
+
+    def __init__(self):
+        self.wd = WebDriver(capabilities={"marionette": False})
+        self.wd.implicitly_wait(60)
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("http://192.168.10.214/addressbook/index.php")
+
+    def login(self, username, password):
+        wd = self.wd
+        self.open_home_page()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath(".//*[@id='LoginForm']/input[@value='Login']").click()
+
+    def open_groups_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("groups").click()
+
+    def create_group(self, group):
+        wd = self.wd
+        self.open_groups_page()
+        wd.find_element_by_name("new").click()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        wd.find_element_by_name("submit").click()
+        self.return_to_group_page(wd)
+
+    def return_to_group_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
+
+    def logout(self):
+        wd = self.wd
+        wd.find_element_by_link_text("Logout").click()
+
+    def open_contacts_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("add new").click()
+
+    def create_contact(self, contact):
+        wd = self.wd
+        self.open_contacts_page()
+        for attr, value in contact.__dict__.items():
+            if (str(attr) == 'bday') or (str(attr) == 'bmonth'):
+                wd.find_element_by_xpath(
+                    "//select[@name='" + value.xpath + "']/*[@value='" + value.value + "']").click()
+                continue
+            wd.find_element_by_name(value.xpath).send_keys(value.value)
+        wd.find_element_by_name("submit").click()
+
+    def destroy(self):
+        self.wd.quit()
