71517fd07b5fa1559313326df044610a0e1cabc6
Pavel Senin <psenin@mail.ru>
2017-08-12 19:26:10 +0300

    minor

--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -4,5 +4,5 @@ from model.group import Group
 
 def test_modify_first_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify_first_group(Group(name="python_test_group", header="python_test_group_Logo", footer="python_test_group_Comment"))
+    app.group.modify_first_group(Group(name="python_test_group2", header="python_test_group_Logo2", footer="python_test_group_Comment2"))
     app.session.logout()
