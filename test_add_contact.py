a37f6e38c5bff8de620659057dcf689d78afd252
Pavel Senin <psenin@mail.ru>
2017-08-12 15:45:52 +0300

    move methods to fixture class, add using py.test, remove class

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,64 +1,18 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
+from application import Application
+import pytest
 from contact import Contact
-import unittest
 
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-class test_add_contact(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver(capabilities={"marionette": False})
-        self.wd.implicitly_wait(60)
-
-    def create_contact(self, contact):
-        wd = self.wd
-        self.open_contacts_page()
-        for attr, value in contact.__dict__.items():
-            if (str(attr) == 'bday') or (str(attr) == 'bmonth'):
-                wd.find_element_by_xpath(
-                    "//select[@name='" + value.xpath + "']/*[@value='" + value.value + "']").click()
-                continue
-            wd.find_element_by_name(value.xpath).send_keys(value.value)
-        wd.find_element_by_name("submit").click()
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://192.168.10.214/addressbook/index.php")
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath(".//*[@id='LoginForm']/input[@value='Login']").click()
-
-    def open_contacts_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-
-    def test_add_contact(self):
-        success = True
-        wd = self.wd
-        self.login(username="admin", password="secret")
-        self.create_contact(Contact(firstname="Senin", lastname="Pavel", email="psenin@mail.ru", bday="24",
-                                        bmonth="December", byear="1981"))
-        self.logout()
-        self.assertTrue(success)
-
-    def tearDown(self):
-        self.wd.quit()
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_add_contact(app):
+    app.login(username="admin", password="secret")
+    app.create_contact(Contact(firstname="Senin", lastname="Pavel", email="psenin@mail.ru", bday="24",
+                                    bmonth="December", byear="1981"))
+    app.logout()
--- test_add_group.py
+++ test_add_group.py
@@ -1,71 +1,23 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
 from group import Group
-import unittest
+import pytest
+from application import Application
 
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-class test_add_group(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver(capabilities={"marionette": False})
-        self.wd.implicitly_wait(60)
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="python_test_group", header="python_test_group_Logo", footer="python_test_group_Comment"))
+    app.logout()
 
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://192.168.10.214/addressbook/index.php")
 
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath(".//*[@id='LoginForm']/input[@value='Login']").click()
-
-    def open_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        self.open_groups_page()
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_name("submit").click()
-        self.return_to_group_page(wd)
-
-    def return_to_group_page(self, wd):
-        wd.find_element_by_link_text("group page").click()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-
-    def test_add_group(self):
-        success = True
-        wd = self.wd
-        self.login(username="admin", password="secret")
-        self.create_group(Group(name="python_test_group", header="python_test_group_Logo", footer="python_test_group_Comment"))
-        self.logout()
-        self.assertTrue(success)
-
-    def test_add_empty_group(self):
-        success = True
-        self.login( username="admin", password="secret")
-        self.create_group(Group(name="", header="", footer=""))
-        self.logout()
-        self.assertTrue(success)
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == '__main__':
-    unittest.main()
+def test_add_empty_group(app):
+    app.login( username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
