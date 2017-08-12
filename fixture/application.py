c16c3a997e563b7a3e958ec98edd6abe10f7b6be
Pavel Senin <psenin@mail.ru>
2017-08-12 16:18:25 +0300

    create class GroupHelper

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixture.session import SessionHelper
+from fixture.group import GroupHelper
 
 
 class Application:
@@ -8,28 +9,11 @@ class Application:
         self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
         self.session = SessionHelper (self)
+        self.group = GroupHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://192.168.10.214/addressbook/index.php")
-
-    def open_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        self.open_groups_page()
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_name("submit").click()
-        self.return_to_group_page(wd)
-
-    def return_to_group_page(self, wd):
-        wd.find_element_by_link_text("group page").click()
-
 
     def open_contacts_page(self):
         wd = self.wd
---
+++ fixture/group.py
@@ -0,0 +1,22 @@
+class GroupHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+
+    def open_groups_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("groups").click()
+
+    def create(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        wd.find_element_by_name("new").click()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        wd.find_element_by_name("submit").click()
+        self.return_to_group_page(wd)
+
+    def return_to_group_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
