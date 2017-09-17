1ccc29e472603abc1ee2534e1c45fc24b8b5ef12
Pavel Senin <psenin@mail.ru>
2017-09-17 22:33:42 +0300

    add connection to db and --check_ui for tests_groups

--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -1,21 +1,31 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-from random import randrange
+import random
 
-def test_modify_first_group_name(app):
-    if app.group.count() == 0:
+
+def test_modify_random_group_name(app, db, check_ui):
+    if len(db.get_group_list()) == 0:
         app.group.create(Group(name="test"))
-    old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))
-    group = Group(name="New group")
-    group.id = old_groups[index].id
-    app.group.modify_group_by_index(group, index)
-    assert len(old_groups) == app.group.count()
-    new_groups = app.group.get_group_list()
-    print(old_groups)
-    old_groups[index] = group
-    print(old_groups)
+    # old_groups = app.group.get_group_list()
+    old_groups = db.get_group_list()
+    # index = randrange(len(old_groups))
+    group_for_modify = random.choice(old_groups)
+    group = Group(name="New group for test_modify_random_group_name")
+    app.group.modify_group_by_id(group, group_for_modify.id)
+    # assert len(old_groups) == app.group.count()
+    # new_groups = app.group.get_group_list()
+
+    new_groups = db.get_group_list()
+    assert len(old_groups) == len(new_groups)
+
+    old_groups[old_groups.index(group_for_modify)] = new_groups[old_groups.index(group_for_modify)]
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
+    if check_ui:
+        def clean(group):
+            return Group(id=group.id, name=group.name.strip())
+        db_list = map(clean, new_groups)
+        assert sorted(db_list, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
 
 
 # def test_modify_first_group_header(app):
